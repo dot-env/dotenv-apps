869641807f51ff453e4f62b11fc74996e8125f4c
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-13 21:59:40 +0200

    feat: add new service pages for CTO Consulting, DevOps & Cloud Engineering, Quality Assurance, and Scalable Software Architecture with metadata

---
+++ website/src/app/cto-consulting/page.tsx
@@ -0,0 +1,19 @@
+import { generateTitle } from "#/configs/site";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: generateTitle("Services - CTO Consulting"),
+    description: "CTO Consulting offers a wide range of services to help you with your technology needs.",
+};
+
+export default function page() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>
+                This is the about us page.
+            </p>
+        </div>
+    );
+}
